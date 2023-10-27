600e3e0645b353352a36d1dfeebc691010ca3236
amira-creator <amira.abdelfattah99@gmail.com>
2023-10-27 08:32:10 -0700

    a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # Using Puppet, install flask from pip3.
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
