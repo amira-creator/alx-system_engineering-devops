08663c288b2a34122f11cb8f1b0a2a71c654cb0d
amira-creator <amira.abdelfattah99@gmail.com>
2023-10-27 08:55:15 -0700

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
 # Using Puppet, install flask from pip3.
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
